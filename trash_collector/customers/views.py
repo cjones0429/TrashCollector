0bfb0ee15618c11773949f359e3b1c5fc509681a
unknown <mrchrisfarrell@yahoo.com>
2021-06-04 14:12:37 -0400

    added customer.create method

--- trash_collector/customers/views.py
+++ trash_collector/customers/views.py
@@ -1,6 +1,7 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from .models import Customer
+from django.urls import reverse
 # Create your views here.
 
 # TODO: Create a function for each path created in customers/urls.py. Each will need a template as well.
@@ -14,3 +15,17 @@ def index(request):
     # thereby finding the customer/employee profile that matches with the logged-in user.
     print(user)
     return render(request, 'customers/index.html')
+
+
+def create(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        address = request.POST.get('address')
+        pickup_day = request.POST.get('pickup_day')
+        zip_code = request.POST.get('zip_code')
+        one_time_pickup = request.POST.get('one_time_pickup')
+        new_customer = Customer(name=name, address=address, zip_code=zip_code, pickup_day=pickup_day, one_time_pickup=one_time_pickup)
+        new_customer.save()
+        return HttpResponseRedirect(reverse('customers:details'))
+    else:
+        return render(request, 'customers/create.html')
