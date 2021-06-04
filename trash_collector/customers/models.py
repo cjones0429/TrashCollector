e1b3c98d8441070923c97ed74fe04b5c519521ae
unknown <mrchrisfarrell@yahoo.com>
2021-06-04 12:45:23 -0400

    migrated customer model

--- trash_collector/customers/models.py
+++ trash_collector/customers/models.py
@@ -5,12 +5,12 @@ from django.db import models
 
 
 class Customer(models.Model):
-    name = models.CharField(max_length=50)
-    pickup_day = models.CharField(max_length=50)
-    address = models.CharField(max_length=50)
-    zip_code = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, default=("temp"))
+    pickup_day = models.CharField(max_length=50, default="Monday")
+    address = models.CharField(max_length=50, null=True)
+    zip_code = models.CharField(max_length=50, null=True)
     account_balance = models.IntegerField(default=0)
-    one_time_pickup = models.DateField
-    suspension_start = models.DateField
-    suspension_end = models.DateField
+    one_time_pickup = models.DateField(null=True)
+    suspension_start = models.DateField(null=True)
+    suspension_end = models.DateField(null=True)
     user = models.ForeignKey('accounts.User', blank=True, null=True, on_delete=models.CASCADE)
