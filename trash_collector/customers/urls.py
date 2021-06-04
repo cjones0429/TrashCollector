b94bffcbf6a19ec920a99fe8f454a5515342eafb
cjones0429 <cejones1997@aim.com>
2021-06-04 15:00:09 -0400

    added urlpattern paths in customers url.py

--- trash_collector/customers/urls.py
+++ trash_collector/customers/urls.py
@@ -7,4 +7,10 @@ from . import views
 app_name = "customers"
 urlpatterns = [
     path('', views.index, name="index"),
+    path('create/', views.create, name="create"),
+    path('account_info_details/', views.account_info_details, name="account_info_details"),
+    path('pickup_day/', views.pickup_day, name="pickup_day"),
+    path('one_time_pickup/', views.one_time_pickup, name = "one_time_pickup"),
+    path('suspend_pickup/', views.suspend_pickup, name="suspend_pickup"),
+
 ]
