4957701fddf7ec30fce2d423f261cd2c68cdd1d3
unknown <mrchrisfarrell@yahoo.com>
2021-06-04 12:31:39 -0400

    updated customers.model

--- trash_collector/customers/models.py
+++ trash_collector/customers/models.py
@@ -6,4 +6,11 @@ from django.db import models
 
 class Customer(models.Model):
     name = models.CharField(max_length=50)
+    pickup_day = models.CharField(max_length=50)
+    address = models.CharField(max_length=50)
+    zip_code = models.CharField(max_length=50)
+    account_balance = models.IntegerField(default=0)
+    one_time_pickup = models.DateField
+    suspension_start = models.DateField
+    suspension_end = models.DateField
     user = models.ForeignKey('accounts.User', blank=True, null=True, on_delete=models.CASCADE)
